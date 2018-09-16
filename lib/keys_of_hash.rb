2b630451726b97d896d895ecdb434717ac3befc5
patrickp246 <patrickp246@gmail.com>
2018-09-16 09:58:28 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,10 @@ class Hash
   def keys_of(something, *vals)
     rtn_keys = []
     vals.each do |v|
-      rtn_keys <<
+      if something.key(v) != nil
+        rtn_keys << v
+      end
     end
+    rtn_keys
   end
 end
